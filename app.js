98ad809be478f2b805d71e7cb22f0c7c7935361d
Wilson Chang <wilsonchangjy@gmail.com>
2024-08-22 23:25:32 +0800

    CORS Fix

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ app.use(express.raw({type: '*/*'}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api);
-app.use('/ascii', express.static('ascii'));
-app.use('/tarot.json', express.static('tarot.json'));
 
 app.set('etag', false);
 app.use((request, response, next) => {
